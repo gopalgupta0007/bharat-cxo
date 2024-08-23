934542416ccbfd7342e7b536577767c9716e3968
Gopal Gupta <105534501+gopalgupta0007@users.noreply.github.com>
2024-08-23 13:31:51 +0530

    create api for the article page

--- backend/src/router/cxoRouter.js
+++ backend/src/router/cxoRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {getcxoData, updateCxoData} = require("../controller/cxoController");
+const {getcxoData, updateCxoData, updateCxoArticlesData} = require("../controller/cxoController");
 const cxoRouter = express.Router();
 // const getcxoData = require("");
 
 cxoRouter.get("/vivek.pravin@2324/getallcxodata", getcxoData)
 cxoRouter.patch("/vivek.pravin@2324/data/update/updatecxodata", updateCxoData)
+cxoRouter.patch("/vivek.pravin@2324/data/update/articles/updatecxodata", updateCxoArticlesData)
 
 module.exports = cxoRouter;
